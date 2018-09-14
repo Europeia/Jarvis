353f3d913ebe0eeb0be07d945795582d5f3720f1
Darcness <darc@outlook.com>
2018-09-14 03:26:15 -0700

    Adding new greeting message commands (#10)

--- config.py
+++ config.py
@@ -1,94 +1,111 @@
 import discord
 import json
 
+
 class ConfigManager:
     configData = None
-    
+
     def __init__(self):
         self.readConfig()
-        
-    #IO Operations
+
+    # IO Operations
     def readConfig(self):
         try:
             f = open('data/config.json')
             self.configData = json.load(f)
         except:
             self.configData = {}
-        
+
     def writeConfig(self):
         f = open('data/config.json',  'w')
         json.dump(self.configData, f, indent=2)
-        
-    #DB Manipulation        
+
+    # DB Manipulation
     def updateServerData(self, server: discord.Guild):
-        serverData = self.configData[str(server.id)] if self.configData.get(str(server.id)) is not None else {}
+        serverData = self.configData[str(server.id)] if self.configData.get(
+            str(server.id)) is not None else {}
         serverData['name'] = server.name
         self.configData[str(server.id)] = serverData
         self.__updateRolesData(server)
-        
+
     def __updateRolesData(self,  server: discord.Guild):
         serverData = self.configData[str(server.id)]
-        serverData['roleData'] = serverData['roleData'] if serverData.get('roleData') is not None else {}
-        newServerData = {}
-        newServerData['roleData'] = {}
+        serverData['roleData'] = serverData['roleData'] if serverData.get(
+            'roleData') is not None else {}
+        newRolesData = {}
         for iRole in range(len(server.roles)):
             role = server.roles[iRole]
             if not role.is_default():
-                roleData = serverData['roleData'][str(role.id)] if serverData['roleData'].get(str(role.id)) is not None else {}
+                roleData = serverData['roleData'][str(role.id)] if serverData['roleData'].get(
+                    str(role.id)) is not None else {}
                 newRoleData = {}
                 newRoleData['name'] = role.name
-                newRoleData['commanders'] = roleData['commanders'] if roleData.get('commanders') is not None else {}
+                newRoleData['commanders'] = roleData['commanders'] if roleData.get(
+                    'commanders') is not None else {}
                 for memberId in newRoleData['commanders']:
                     member = server.get_member(int(memberId))
                     if member is not None:
-                        newRoleData['commanders'][memberId] = self.__formatMemberName(member)
-                newServerData['roleData'][str(role.id)] = newRoleData
-        self.configData[str(server.id)] = newServerData
-    
+                        newRoleData['commanders'][memberId] = self.__formatMemberName(
+                            member)
+                newRolesData[str(role.id)] = newRoleData
+        self.configData[str(server.id)]['roleData'] = newRolesData
+
     def addCommander(self,  server: discord.Guild,  role: discord.Role,  member: discord.Member):
-        commanderList = self.__getCommanderList(self.__getRole(self.__getRoleList(self.configData.get(str(server.id))), str(role.id)))
+        commanderList = self.__getCommanderList(self.__getRole(
+            self.__getRoleList(self.configData.get(str(server.id))), str(role.id)))
         if commanderList is not None:
             commanderList[str(member.id)] = self.__formatMemberName(member)
-    
+
     def remCommander(self,  server: discord.Guild,  role: discord.Role,  member: discord.Member):
         if self.isCommander(server,  role,  member):
-            del self.configData[str(server.id)]['roleData'][str(role.id)]['commanders'][str(member.id)]
-    
+            del self.configData[str(server.id)]['roleData'][str(
+                role.id)]['commanders'][str(member.id)]
+
     def listCommanders(self,  server: discord.Guild,  role: discord.Role):
         output = ''
-        commanderList = self.__getCommanderList(self.__getRole(self.__getRoleList(self.configData.get(str(server.id))), str(role.id)))
+        commanderList = self.__getCommanderList(self.__getRole(
+            self.__getRoleList(self.configData.get(str(server.id))), str(role.id)))
         for commander in commanderList:
             output += commanderList[commander] + '\n'
-        return output;
-    
+        return output
+
     def isCommander(self,  server: discord.Guild,  role: discord.Role,  member: discord.Member):
-        commander = self.__getCommander(self.__getCommanderList(self.__getRole(self.__getRoleList(self.configData.get(str(server.id))), str(role.id))), str(member.id))
+        commander = self.__getCommander(self.__getCommanderList(self.__getRole(
+            self.__getRoleList(self.configData.get(str(server.id))), str(role.id))), str(member.id))
         if commander is not None:
             return True
         return False
-    
-    #Private Model Utility Functions
+
+    def setGreetingMessage(self,  server: discord.Guild,  greetingMessage):
+        self.configData[str(server.id)]['greetingMessage'] = greetingMessage
+
+    def getGreetingMessage(self,  server: discord.Guild):
+        if self.configData[str(server.id)].get('greetingMessage') is not None:
+            return self.configData[str(server.id)]['greetingMessage']
+        return 'none'
+
+    # Private Model Utility Functions
     def __getCommander(self, commanderList,  commanderId):
         if commanderList is not None and len(commanderList) > 0:
             return commanderList.get(commanderId)
         return None
-    
+
     def __getCommanderList(self, roleData):
         if roleData is not None:
             return roleData.get('commanders')
         return None
-    
+
     def __getRole(self, roleList, roleId):
         if roleList is not None and len(roleList) > 0:
             return roleList.get(roleId)
         return None
-    
+
     def __getRoleList(self, serverData):
         if serverData is not None:
             return serverData.get('roleData')
         return None
-    
 
-    #Utility Functions      
+    # Utility Functions
+
     def __formatMemberName(self,  member: discord.Member):
         return member.name + '@#' + member.discriminator
--- jarvis.py
+++ jarvis.py
@@ -1,3 +1,5 @@
+import discord
+
 from discord.ext import commands
 
 from config import ConfigManager
@@ -7,33 +9,50 @@ description = "J.A.R.V.I.S is an administration helper."
 bot = commands.Bot(command_prefix='!', description=description)
 configManager = None
 
+
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
-    print('------')
+    print('------ Servers ------')
     global configManager
     configManager = ConfigManager()
     updateConfigs()
     AdministrationCommands(bot,  configManager)
 
+@bot.event
+async def on_message(message: discord.Message):
+    # do stuff
+    if message.type == discord.MessageType.new_member : # Send a Greeting?
+        guild = message.guild
+        channel = message.channel
+        author = message.author
+        greeting = configManager.getGreetingMessage(guild)
+        if greeting != 'none' :
+            await channel.send(greeting.replace('@@NAME@@', author.mention))
+
+    await bot.process_commands(message)
 
 def updateConfigs():
     servers = list(bot.guilds)
     global configManager
     for iServer in range(len(servers)):
-        configManager.updateServerData(servers[iServer])
+        server = servers[iServer]
+        configManager.updateServerData(server)
+        print('Updated Data for ' + server.name + ':' + str(server.id))
     configManager.writeConfig()
-   
+    print('Updated Config Data Written!')
+
+
 @bot.command()
 @commands.is_owner()
 async def shutdown(ctx):
     await ctx.message.author.send('Shutting Down!')
-    bot.logout()
+    await bot.logout()
     exit()
 
-try:    
+try:
     f = open('token.txt',  'r')
 except:
     print('Unable to find token.txt!')
